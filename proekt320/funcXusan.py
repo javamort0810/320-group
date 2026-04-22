ba1fe77bc5da2ed77b2dc489a447ec800f937174
Husanov_Javohir <116440078+javamort0810@users.noreply.github.com>
2026-04-22 12:22:25 +0500

    Add files via upload

---
+++ proekt320/funcXusan.py
@@ -0,0 +1,8 @@
+
+def funk320Xusan(number,pumber):
+    c = 0
+    for x in number:
+        if x < pumber:
+            c += 1
+    return c
+
---
+++ proekt320/main.py
@@ -0,0 +1,5 @@
+from funcXusan import funk320Xusan
+a = [91,38,2,3,54]
+b = 47
+result = funk320Xusan(a,b)
+print(f"Числа меньше {b}: {result}")
